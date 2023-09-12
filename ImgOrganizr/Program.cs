d76d33f20ddeda8e0b89a0ab5d349c2602ab3308
ARGO492 <82467127+ARGO492@users.noreply.github.com>
2023-09-12 22:32:47 +0200

    Added Jpeg support

--- ImgOrganizr/Program.cs
+++ ImgOrganizr/Program.cs
@@ -1,27 +1,28 @@
 ﻿namespace ImgOrganizr
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Globalization;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Spectre.Console;
 
-    class Program
-    {
-        /// <summary>
-        /// The entry point for the program.
-        /// </summary>
+	class Program
+	{
+		/// <summary>
+		/// The entry point for the program.
+		/// </summary>
 		/// <param name="args">CLI arguments. The first is one of the commands --rename, --move and the second is directory, the third is the regex depending on the chosen command.</param>
-        static void Main(string[] args)
-        {
-            if (args.Length < 2)
-            {
-                AnsiConsole.MarkupLine("[red]Error: Please provide the directory and at least one operation (--rename, --move).[/]");
-                return;
-            }
+		static void Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				AnsiConsole.MarkupLine("[red]Error: Please provide the directory and at least one operation (--rename, --move).[/]");
+				return;
+			}
 
             DisplayBanner();
 
@@ -30,11 +31,11 @@
 			string dir = args[1];
 			string regexPattern = args[2];
 
-            if (!Directory.Exists(dir))
-            {
-                AnsiConsole.MarkupLine($"[red]Error: The directory '{dir}' does not exist.[/]");
-                return;
-            }
+			if (!Directory.Exists(dir))
+			{
+				AnsiConsole.MarkupLine($"[red]Error: The directory '{dir}' does not exist.[/]");
+				return;
+			}
 
 			if (String.IsNullOrEmpty(regexPattern))
 			{
@@ -42,18 +43,16 @@
 				return;
 			}
 
-            if (operations.Contains("--rename"))
-            {
+			if (operations.Contains("--rename"))
+			{
 				RenameFiles(dir, regexPattern);
-            }
+			}
 
-            if (operations.Contains("--move"))
-            {
+			if (operations.Contains("--move"))
+			{
 				MoveFiles(dir, regexPattern);
-            }
-
-			return;
-        }
+			}
+		}
 
         /// <summary>
         /// Displays the application's banner.
@@ -67,24 +66,24 @@
             );
         }
 
-        /// <summary>
+		/// <summary>
 		/// Extracts 'Date Taken' from the metadata of the image file.
-        /// </summary>
+		/// </summary>
 		/// <param name="filePath">Path to the image file.</param>
 		/// <returns>Date taken as DateTime or null.</returns>
 		static DateTime? ExtractDateTaken(string filePath, string regexPattern)
-        {
-            try
-            {
-                using (Image image = Image.FromFile(filePath))
-            {
-                var propItem = image.GetPropertyItem(36867); // 36867 is the id for 'Date Taken'
-                string dateTakenStr = Encoding.UTF8.GetString(propItem.Value).Trim('\0');
-                return DateTime.ParseExact(dateTakenStr, "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture);
-            }
-            }
+		{
+			try
+			{
+				using (Image image = Image.FromFile(filePath))
+				{
+					var propItem = image.GetPropertyItem(36867); // 36867 is the id for 'Date Taken'
+					string dateTakenStr = Encoding.UTF8.GetString(propItem.Value).Trim('\0');
+					return DateTime.ParseExact(dateTakenStr, "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture);
+				}
+			}
 			catch
-            {
+			{
 				// Fallback: Try to extract date from filename using regex
 				if (!string.IsNullOrEmpty(regexPattern))
 				{
@@ -98,84 +97,82 @@
 					}
 				}
 
-                return null;
-            }
-        }
+				return null;
+			}
+		}
 
-        /// <summary>
+		/// <summary>
 		/// Renames files in the specified directory. 
-        /// </summary>
+		/// </summary>
 		/// <param name="dir">Directory path</param>
 		static void RenameFiles(string dir, string regexPattern)
-        {
-            var uniqueNumbersPerDay = new Dictionary<string, int>();
-            var table = new Table().Border(TableBorder.Rounded);
-            table.AddColumn("Old File Name");
-            table.AddColumn("New File Name");
+		{
+			var uniqueNumbersPerDay = new Dictionary<string, int>();
+			var table = new Table().Border(TableBorder.Rounded);
+			table.AddColumn("Old File Name");
+			table.AddColumn("New File Name");
 
-            // Create LiveDisplay
-            var live = AnsiConsole.Live(table);
-
-            live.Start(ctx =>
-            {
-                foreach (var filePath in Directory.GetFiles(dir, "*.jpg"))
-                {
-                    string oldFileName = Path.GetFileName(filePath);
+			var live = AnsiConsole.Live(table);
+			live.Start(ctx =>
+			{
+				string[] searchPatterns = { "*.jpg", "*.jpeg" };
+				foreach (string filePath in searchPatterns.SelectMany(sp => Directory.GetFiles(dir, sp)))
+				{
+					string oldFileName = Path.GetFileName(filePath);
 					DateTime? dateTaken = ExtractDateTaken(filePath, regexPattern);
 
-                    if (dateTaken != null)
-                    {
-                        string dateTakenFormatted = dateTaken.Value.ToString("dd_MM_yyyy");
+					if (dateTaken != null)
+					{
+						string dateTakenFormatted = dateTaken.Value.ToString("dd_MM_yyyy");
 
-                        if (!uniqueNumbersPerDay.ContainsKey(dateTakenFormatted))
-                        {
-                            uniqueNumbersPerDay[dateTakenFormatted] = 1;
-                        }
-                        else
-                        {
-                            uniqueNumbersPerDay[dateTakenFormatted]++;
-                        }
+						if (!uniqueNumbersPerDay.ContainsKey(dateTakenFormatted))
+						{
+							uniqueNumbersPerDay[dateTakenFormatted] = 1;
+						}
+						else
+						{
+							uniqueNumbersPerDay[dateTakenFormatted]++;
+						}
 
-                        string uniqueNumber = uniqueNumbersPerDay[dateTakenFormatted].ToString("D4");
-                        string newFileName = $"{dateTakenFormatted}_{uniqueNumber}_image.jpg";
-                        string newFilePath = Path.Combine(Path.GetDirectoryName(filePath), newFileName);
+						string uniqueNumber = uniqueNumbersPerDay[dateTakenFormatted].ToString("D4");
+						string newFileName = $"{dateTakenFormatted}_{uniqueNumber}_image.jpg";
+						string newFilePath = Path.Combine(Path.GetDirectoryName(filePath), newFileName);
 
-                        File.Move(filePath, newFilePath);
-                        table.AddRow(oldFileName, newFileName);
-                    }
-                    else
-                    {
+						File.Move(filePath, newFilePath);
+						table.AddRow(oldFileName, newFileName);
+					}
+					else
+					{
 						// Keep the original name if DateTaken is null
-                        table.AddRow(oldFileName, "Kept Original");
-                    }
+						table.AddRow(oldFileName, "Kept Original");
+					}
 
-                    // Update the LiveDisplay
-                    ctx.Refresh();
-                }
-            });
-        }
+					// Update the LiveDisplay
+					ctx.Refresh();
+				}
+			});
+		}
 
-        /// <summary>
+		/// <summary>
 		/// Moves files into subdirectories based on their 'Date Taken'.
-        /// </summary>
+		/// </summary>
 		/// <param name="dir">Directory path</param>
 		static void MoveFiles(string dir, string regexPattern)
-        {
-            foreach (var filePath in Directory.GetFiles(dir, "*.jpg"))
-            {
+		{
+			string[] searchPatterns = { "*.jpg", "*.jpeg" };
+			foreach (string filePath in searchPatterns.SelectMany(sp => Directory.GetFiles(dir, sp)))
+			{
 				DateTime? dateTaken = ExtractDateTaken(filePath, regexPattern);
 
 				if (dateTaken != null)
 				{
 					string newDir = Path.Combine(dir, dateTaken.Value.Year.ToString(), dateTaken.Value.Month.ToString("D2"), dateTaken.Value.Day.ToString("D2"));
-                Directory.CreateDirectory(newDir);
+					Directory.CreateDirectory(newDir);
 
-                string newFilePath = Path.Combine(newDir, Path.GetFileName(filePath));
-                File.Move(filePath, newFilePath);
-            }
-        }
-    }
+					string newFilePath = Path.Combine(newDir, Path.GetFileName(filePath));
+					File.Move(filePath, newFilePath);
+				}
+			}
+		}
 	}
-}
-
 }
