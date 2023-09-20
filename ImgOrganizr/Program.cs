fe6593238e3e51c7723e14a055eaca708e9b32dd
ARGO492 <82467127+ARGO492@users.noreply.github.com>
2023-09-20 15:43:26 +0200

    Formatted the code

--- ImgOrganizr/Program.cs
+++ ImgOrganizr/Program.cs
@@ -1,15 +1,15 @@
 ﻿namespace ImgOrganizr
 {
-using System;
-using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
 	using System.Drawing;
 	using System.Drawing.Imaging;
 	using System.Globalization;
 	using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using Spectre.Console;
+	using System.Linq;
+	using System.Text;
+	using System.Text.RegularExpressions;
+	using Spectre.Console;
 
 	class Program
 	{
